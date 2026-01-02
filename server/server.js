974f633c4077fc306cbe4061317a8141a8026b38
konda sridhar <kondasridhar628@gmail.com>
2026-01-02 23:08:19 +0530

    Fixed Express 5 wildcard route for production

--- server/server.js
+++ server/server.js
@@ -43,7 +43,7 @@ const path = require('path');
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/dist')));
 
-    app.get('*', (req, res) => {
+    app.get(/(.*)/, (req, res) => {
         res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'));
     });
 } else {
